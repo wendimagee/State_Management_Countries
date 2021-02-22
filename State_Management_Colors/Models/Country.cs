2db60a084434cb60f01e30cc056603f1aff6e1a0
Wendi Magee <wendimagee@gmail.com>
2021-02-22 14:22:55 -0500

    Add project files.

---
+++ State_Management_Colors/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace State_Management_Colors.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string[] OfficialLanguages { get; set; }
+
+        public string HelloWorld { get; set; }
+
+        public string Description { get; set; }
+        public string[] NationalColors { get; set; }
+
+    }
+}
---
+++ State_Management_Colors/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace State_Management_Colors.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
